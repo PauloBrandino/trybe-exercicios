f444f5ebe3950e73dec5e5cb4b0531b0c97e8b80
Paulo <contatobrandino@gmail.com>
2022-09-21 19:09:28 -0300

    Switch e Case

---
+++ fundamentos/secao-01-introducao-js/01-js-primeiros-passos/if/switchCase.js
@@ -0,0 +1,15 @@
+let canditado = "Lista";
+
+switch (canditado) {
+    case "Aprovado":
+        console.log ("Parabéns, você foi aprovada(o)!");
+        break;
+    case "Lista":
+        console.log ("Você está na nossa lista de espera");
+        break
+    case "Reprovado":
+        console.log("Você foi reprovada(o)");
+        break
+    default: 
+        console.log("Informação incorreta");
+}
