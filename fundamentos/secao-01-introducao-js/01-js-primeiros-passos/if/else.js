7518fa7f684033243b57787daf4899dd43dc84ca
Paulo <contatobrandino@gmail.com>
2022-09-21 18:51:00 -0300

    operadores logicos

--- fundamentos/secao-01-introducao-js/01-js-primeiros-passos/if/else.js
+++ fundamentos/secao-01-introducao-js/01-js-primeiros-passos/if/else.js
@@ -1,9 +1,9 @@
-const nota = 30;
+const nota = 90;
 
 if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!")
-} else if (nota >= 60 < 80) {
-    console.log ("Você está na nossa lista de espera")
-} else (nota < 60); {
-    console.log ("Você foi reprovado(a)!")  
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota >= 60 && nota < 80) {
+    console.log ("Você está na nossa lista de espera");
+} else {
+    console.log ("Você foi reprovado(a)!"); 
 }
