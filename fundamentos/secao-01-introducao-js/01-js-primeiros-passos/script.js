25238a348df1c9725840c8fd9302b25fa9460e99
Paulo <contatobrandino@gmail.com>
2022-09-21 16:10:50 -0300

    Operadores Aritiméticos

--- fundamentos/secao-01-introducao-js/01-js-primeiros-passos/script.js
+++ fundamentos/secao-01-introducao-js/01-js-primeiros-passos/script.js
@@ -1,9 +1,7 @@
-const myName = "Paulo";
-const birthCity = "Piraju";
-let birthYear = 1994;
+const base = 5;
+const heigth = 8;
+const area = base * heigth
+const perimeter = (base *2) + (heigth *2)
 
-birthYear = 2030;
-
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+console.log(area);
+console.log(perimeter);
