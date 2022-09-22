071d5f601ad3d7e71388c1ec04e5c0f1e1a76164
Paulo <contatobrandino@gmail.com>
2022-09-22 11:50:41 -0300

    calcula Lucro

---
+++ fundamentos/secao-01-introducao-js/01-js-primeiros-passos/if/calculaLucro.js
@@ -0,0 +1,7 @@
+const custo = null;
+const valorVenda = 50;
+
+let custoTotal = custo + (custo*0,2);
+let lucro = valorVenda - custoTotal;
+
+console.log("Vendendo 1000 prodrutos a empresa terá de lucro :", lucro*1000)
