358e76838e1f37e7b0c9bb1eb31c262f87425aec
Paulo <contatobrandino@gmail.com>
2022-09-22 18:53:15 -0300

    Utilizando For com IF/ELSE

--- fundamentos/04-introducao-js/02-JS-array-loop-for/execicio01.js
+++ fundamentos/04-introducao-js/02-JS-array-loop-for/execicio01.js
@@ -6,10 +6,31 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // }
 
 // Soma de todos os valores do array.
-    let somaValores = 0;
+    // let somaValores = 0;
 
-    for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
-        somaValores = somaValores + numbers[indexNumbers];
+    // for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    //     somaValores = somaValores + numbers[indexNumbers];
+    // }
+    // console.log(somaValores);
+
+// Calculando a média dos valores.
+    // let mediaNumbers = 0;
+    // mediaNumbers = somaValores / numbers.length;
+    //console.log(mediaNumbers);
+
+// Utilizando IF pra comparações.
+
+    // if (mediaNumbers > 20) {
+    //     console.log("Valor maior que 20!");
+    // } else {
+    //     console.log("Valor menor que 20!");
+    // }
+
+// Imprimindo maior valor do Array
+    let valorMaior = 0;
+    for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1){
+        if (numbers[indexNumbers] > valorMaior) {
+            valorMaior = numbers[indexNumbers];
+        }
     }
-console.log(somaValores);
-    
+    console.log(valorMaior);
