dffd77816f8fcf60e1b3061ac392a1179d280659
Paulo <contatobrandino@gmail.com>
2022-09-22 19:25:30 -0300

    Finalizado exercicio 01

--- fundamentos/04-introducao-js/02-JS-array-loop-for/execicio01.js
+++ fundamentos/04-introducao-js/02-JS-array-loop-for/execicio01.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Percorrendo todos os valore do Array com console.log.
     // for (indexNumbers =0; indexNumbers < numbers.length; indexNumbers += 1){
@@ -27,10 +27,51 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     // }
 
 // Imprimindo maior valor do Array
-    let valorMaior = 0;
-    for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1){
-        if (numbers[indexNumbers] > valorMaior) {
-            valorMaior = numbers[indexNumbers];
-        }
+    // let valorMaior = 0;
+    // for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1){
+    //     if (numbers[indexNumbers] > valorMaior) {
+    //         valorMaior = numbers[indexNumbers];
+    //     }
+    // }
+    // console.log(valorMaior);
+
+// Quantidade de números ímpares.
+    // let valoresImpares = 0;  
+    // for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1){
+    //     if (numbers[indexNumbers]%2 > 0) {
+    //         valoresImpares += 1
+    //     }       
+    // }
+    
+    // if (valoresImpares > 0) {
+    //     console.log(valoresImpares, "valores ímpares!");
+    // } else {
+    //     console.log("Nenhum número ímpar encontrado!")
+    // }
+          
+
+//Menor valor da array.
+    // let menorValor = Infinity;
+    // for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    //     if (numbers[indexNumbers] < menorValor) {
+    //         menorValor = numbers[indexNumbers]
+    //     }
+    // }
+    // console.log(menorValor);
+    
+//Crianco uma array com for.
+
+    let numbers = [];
+    for (index = 1; index <= 25; index += 1) {
+        numbers.push(index);
     }
-    console.log(valorMaior);
+    console.log(numbers);
+
+    let divi2 = [];
+    for (indexDivi = 0; indexDivi < numbers.length; indexDivi += 1){
+        divi2.push(indexDivi/2)
+    }
+    console.log(divi2);
+
+        
+    
