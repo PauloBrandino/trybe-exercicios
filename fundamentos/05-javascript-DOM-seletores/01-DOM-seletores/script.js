756e9d2cf529116b0376e48ac9e40a53ab07efca
Paulo <contatobrandino@gmail.com>
2022-09-29 18:26:06 -0300

    Finalizado

---
+++ fundamentos/05-javascript-DOM-seletores/01-DOM-seletores/script.js
@@ -0,0 +1,6 @@
+document.getElementById("page-title").innerText = "Lords of the Rings";
+
+let paragrafo = document.getElementById("second-paragraph")
+    paragrafo.innerText = "Rings of Power"
+
+document.getElementById("subtitle").innerText = "Melhor Franquia"
